7a9f536870a1860e95647c2e1696051472aaeb2e
Twixuss <Twixuss@yandex.ru>
2021-09-02 16:47:53 +0300

    first

---
+++ source/main.cpp
@@ -0,0 +1,11 @@
+#define TL_IMPL
+#define TL_MAIN
+#include <tl/common.h>
+#include <tgraphics/tgraphics.h>
+
+using namespace tl;
+namespace tg = tgraphics;
+
+s32 tl_main(Span<Span<utf8>> arguments) {
+	return 0;
+}
